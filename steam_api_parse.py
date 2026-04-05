30cbc024732ec4cb0ad9c6ab3c341a6a5751ac08
Celtokisa <bluesteelll@hotmail.com>
2026-04-05 09:23:48 +0300

    Mark's init

---
+++ steam_api_parse.py
@@ -0,0 +1,41 @@
+import requests
+import json
+from copy import deepcopy
+import pandas as pd
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  
+all_games_url = "https://api.steampowered.com/IStoreService/GetAppList/v1/" 
+
+
+def get_next_dataset(params):
+  return requests.get(all_games_url, params).json()["response"]
+
+
+def get_full_dataset(params):
+  
+  last = get_next_dataset(params)
+  acc = deepcopy(last["apps"])
+
+  while "have_more_results" in last.keys():
+    last = get_next_dataset({**params, "last_appid": last["last_appid"]})
+    acc += last["apps"]
+    
+    
+  return acc
+
+def parse_steam_api():
+
+  params = {
+     "key": os.environ["API_KEY"],
+     "max_results": 50000,
+     
+  }
+
+  #full_dataset = list(reduce(lambda acc, n: acc + n["apps"], takewhile_inclusive(lambda res: res["have_more_results"], iterate(lambda pr: get_next_dataset({**all_games_params, "last_appid": pr["last_appid"]}), get_next_dataset(all_games_params))), []))
+  
+  data = get_full_dataset(params)
+  pd.DataFrame(data).to_csv("main_data.csv", index=False)
+
+parse_steam_api()
