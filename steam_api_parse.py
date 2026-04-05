4fbfdad8c92a6f97de66f49aa298e7991bfdc60d
Celtokisa <bluesteelll@hotmail.com>
2026-04-05 09:55:25 +0300

    logging via loguru init

--- steam_api_parse.py
+++ steam_api_parse.py
@@ -4,27 +4,42 @@ from copy import deepcopy
 import pandas as pd
 from dotenv import load_dotenv
 import os
+from loguru import logger
 
 load_dotenv()  
+logger.add(".log")
+
+
+
+
+
+
 all_games_url = "https://api.steampowered.com/IStoreService/GetAppList/v1/" 
 
-
+@logger.catch
 def get_next_dataset(params):
   return requests.get(all_games_url, params).json()["response"]
 
-
+@logger.catch
 def get_full_dataset(params):
   
   last = get_next_dataset(params)
   acc = deepcopy(last["apps"])
 
+  
+
   while "have_more_results" in last.keys():
+    logger.success(f"Dataset successfully fetched last game id: {last['last_appid']}")
+
     last = get_next_dataset({**params, "last_appid": last["last_appid"]})
+
     acc += last["apps"]
     
-    
+  logger.success(f"Fetching ended successfully. Total lines: {len(acc)}")  
   return acc
 
+
+@logger.catch
 def parse_steam_api():
 
   params = {
