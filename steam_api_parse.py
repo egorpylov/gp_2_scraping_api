4f611f1a924102e15e32260030cd1d5de7f1ac73
Celtokisa <bluesteelll@hotmail.com>
2026-04-06 22:25:39 +0300

    refactor

--- steam_api_parse.py
+++ steam_api_parse.py
@@ -44,23 +44,22 @@ async def fetch_pub_steam_full(clients, urls, list_params=None):
 
 
 @logger.catch
-async def fetch_all_pub_steam_sources(appids):
+async def fetch_all_pub_steam_sources(appids, urls):
   clients = list(map(lambda p: httpx.AsyncClient(proxy=p), config.PROXIES))
   splitted_ids = pd.DataFrame(np.array_split(appids, len(config.PROXIES)))
 
-  app_details_url_list = splitted_ids.map(lambda x: config.app_details_url.format(appid=x))
+  data = list()
 
-  app_reviews_url_list = splitted_ids.map(lambda x: config.reviews_url.format(appid=x))
-  app_current_online_url_list = splitted_ids.map(lambda x: config.current_online_url.format(appid=x))
-
-  details_data = await fetch_pub_steam_full(clients, app_details_url_list)
-  reviews_data = await fetch_pub_steam_full(clients, app_reviews_url_list)
-  current_online_data = await fetch_pub_steam_full(clients, app_current_online_url_list)
-
-  logger.warning(pd.DataFrame(details_data).shape)
+  for url in urls:
+    
+    url_list = splitted_ids.map(lambda x: config.app_details_url.format(appid=x))
+    current_data = await fetch_pub_steam_full(clients, url_list)
+    data.append(current_data)
+    logger.success(f"Data for url: {url} successfully fetched")
 
 
-  return (details_data, reviews_data, current_online_data)
+  logger.success("List of all data returning...")
+  return data
 
 
 @logger.catch
@@ -83,7 +82,12 @@ def get_full_dataset():
 
 ids = get_full_dataset()["appid"].head(100)
 
-data_tuple = asyncio.run(fetch_all_pub_steam_sources(ids))
-pd.DataFrame(data_tuple[0]).to_json("first_data.csv")
-pd.DataFrame(data_tuple[1]).to_json("second_data.csv")
-pd.DataFrame(data_tuple[2]).to_json("third_data.csv")
+data_tuple = asyncio.run(fetch_all_pub_steam_sources(ids, (
+  config.app_details_url,
+  config.reviews_url,
+  config.current_online_url
+)))
+
+pd.DataFrame(data_tuple[0]).to_json("details_data.csv")
+pd.DataFrame(data_tuple[1]).to_json("reviews_data.csv")
+pd.DataFrame(data_tuple[2]).to_json("current_online_data.csv")
