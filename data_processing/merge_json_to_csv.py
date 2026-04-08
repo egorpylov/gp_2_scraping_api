0ccabcb3b29151057ac0ef98aed97ef2bf448343
nikita-ivanovv <niigivanov@edu.hse.ru>
2026-04-08 13:34:12 +0300

    parse raw dicts

--- data_processing/merge_json_to_csv.py
+++ data_processing/merge_json_to_csv.py
@@ -15,3 +15,23 @@ rev = json.load(open('reviews_data2.json'))
 d4 = json.load(open('month_top_games_data.json'))
 d5 = json.load(open('year_top_games_data.json'))
 d6 = json.load(open('game_ids_names.json'))
+names = {}
+for i, a in d6.get('appid', {}).items():
+    names[str(a)] = d6.get('name', {}).get(i, '')
+
+details = {}
+for p in d1:
+    for a, x in p.items():
+        details[a] = x
+
+online = {}
+for p in d2:
+    for a, x in p.items():
+        try: online[a] = x['response']['player_count']
+        except: pass
+
+reviews = {}
+for p in rev:
+    for a, x in p.items():
+        if type(x) == dict and 'query_summary' in x:
+            reviews[a] = x['query_summary']
